d017773ef42cd4477383b55d57a9e0b1bf129e53
Unknown <guillaume.colletaz01@gmail.com>
2018-03-28 17:46:33 +0200

    [FEAT]photo

---
+++ ProjetIncident.core/Converters/ImageNameToImageSourceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Xamarin.Forms;
+
+namespace ProjetIncident.core.Converters
+{
+    public class ImageNameToImageSourceConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is string)
+			{
+				return ImageSource.FromFile(value as string);
+			}
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+    }
+}
--- ProjetIncident.core/ViewModel/IncidentFormViewModel.cs
+++ ProjetIncident.core/ViewModel/IncidentFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ProjetIncident.core.Commands;
 using ProjetIncident.Core.ViewModel;
+using Xamarin.Forms;
 
 namespace ProjetIncident.Core.ViewModel
 {
@@ -14,7 +15,52 @@ namespace ProjetIncident.Core.ViewModel
 		{
 			get => new DelegateCommand(() =>
 			{
-				Xamarin.Forms.Application.Current.MainPage = new Views.Incidents();
+				Application.Current.MainPage = new Views.Incidents();
+			});
+		}
+
+		public DelegateCommand TakePhoto
+		{
+			get => new DelegateCommand(async () =>
+			{
+				var imageString = "";
+
+				await Plugin.Media.CrossMedia.Current.Initialize();
+
+				if (!Plugin.Media.CrossMedia.Current.IsCameraAvailable)
+				{
+					await Application.Current.MainPage.DisplayAlert("Prise de photo", "Impossible de prendre une photo : votre appareil ne possède pas de caméra.", "OK");
+					return;
+				}
+
+				if (!Plugin.Media.CrossMedia.Current.IsTakePhotoSupported)
+				{
+					await Application.Current.MainPage.DisplayAlert("Prise de photo", "Impossible de prendre une photo : votre appareil ne propose pas la prise de photos.", "OK");
+					return;
+				}
+
+				var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(
+							new Plugin.Media.Abstractions.StoreCameraMediaOptions()
+							{
+								DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Rear,
+								RotateImage = true,
+								CustomPhotoSize = 5,
+								AllowCropping = true,
+								CompressionQuality = 20
+							});
+
+				if (photo != null)
+				{
+					using (var stream = photo.GetStream())
+					{
+						byte[] imageBinary = new byte[stream.Length];
+						stream.Read(imageBinary, 0, (int)stream.Length);
+						imageString = core.Tools.Convert.BytesToBase64String(imageBinary);
+					}
+					System.IO.File.Delete(photo.Path);
+					/*Photos.Add(imageString);
+					SelectedPhoto = imageString;*/
+				}
 			});
 		}
 
